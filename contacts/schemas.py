080258820a158a224c5422114744bb23b0d1b984
kasjan <aufio96@gmail.com>
2023-07-20 17:48:34 +0200

    add validators for account and contact and tests for them

--- contacts/schemas.py
+++ contacts/schemas.py
@@ -16,12 +16,16 @@ class Contact(BaseModel):
         return value
 
     @field_validator('first_name')
-    def validate_first_name(cls, v):
-        return v.capitalize()
+    def validate_first_name(cls, value):
+        if any(char.isdigit() for char in value):
+            raise ValueError('First name cannot contain digits')
+        return value.capitalize()
 
     @field_validator('last_name')
-    def validate_last_name(cls, v):
-        return v.capitalize()
+    def validate_last_name(cls, value):
+        if any(char.isdigit() for char in value):
+            raise ValueError('Last name cannot contain digits')
+        return value.capitalize()
 
 
 class ShowContact(BaseModel):
